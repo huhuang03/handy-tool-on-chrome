121cb41e52f895d5dd505a7656221123dd554fe6
huhuang03 <huhuang03@gmail.com>
2025-06-22 17:20:05 +0800

    up

--- src/background/service_worker.ts
+++ src/background/service_worker.ts
@@ -1,3 +1,32 @@
+import {DEFAULT_OPEN_TAB_TIME_INTERVAL} from '@/bridge/open_tab_list';
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(`request: ${request}, sender: ${sender}`)
+  console.log(`request: ${JSON.stringify(request)}, sender: ${JSON.stringify(sender)}`)
+  const timeInterval = request.timeInterval || DEFAULT_OPEN_TAB_TIME_INTERVAL
+  const links = (request.links ?? []) as string[]
+  if (!links || links.length === 0) {
+    return
+  }
+
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i]
+    setTimeout(() => {
+      getTab(sender.tab?.id, (tab: chrome.tabs.Tab | null) => {
+        const targetIndex = (tab?.index ?? -1) + 1
+        chrome.tabs.create({
+          url: link,
+          index: targetIndex,
+          active: false,
+        }).catch(console.error)
+      })
+    }, i * timeInterval)
+  }
 })
+
+function getTab(tabId: number | null | undefined, callback: (tab: chrome.tabs.Tab | null) => void) {
+  if (!tabId) {
+    callback(null)
+    return
+  }
+  chrome.tabs.get(tabId, callback)
+}
--- src/bridge/open_tab_list.ts
+++ src/bridge/open_tab_list.ts
@@ -1,4 +1,6 @@
-interface OpenTabList {
+export const DEFAULT_OPEN_TAB_TIME_INTERVAL = 200
+
+export interface OpenTabList {
   links: [String],
   /**
    * 打开间隔时间, default is 200
