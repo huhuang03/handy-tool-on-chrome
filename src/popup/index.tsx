80503c1010fef1319b6c54d8fe4eefbed7880971
huhuang03 <huhuang03@gmail.com>
2026-02-26 11:23:45 +0800

    up

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {App} from './App';
 
-const container = document.getElementById('existing-container');
+const container = document.getElementById('app');
 
 if (container) {
   const root = createRoot(container);
